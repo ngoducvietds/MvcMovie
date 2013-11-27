ff6ed702e8e1b39c32d437fa2c03465c91f461b1
Emre Kıyak <lepptin@gmail.com>
2013-11-27 13:45:53 +0200

    Mvc Movie Tutorial
    asp.net mvc tutorial project

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string name, int id = 1) 
+        {
+            ViewBag.ID = id;
+            ViewBag.Message = name;
+            return View();
+            //return HttpUtility.HtmlEncode("Hello " + name + ", num time is : " + id);
+        }
+	}
+}
